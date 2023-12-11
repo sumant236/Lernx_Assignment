00731a187b06c2673f9133510c18760346616a13
Sumant236 <sumantsingh236@gmail.com>
2023-12-11 12:21:09 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./layout/Layout";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route path="/">
-        <Layout />
-      </Route>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+    </Routes>
   );
 }
 
